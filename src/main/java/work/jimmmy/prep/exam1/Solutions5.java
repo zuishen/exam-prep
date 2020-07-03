a92e02b53b7c6b39c66450a7b91719fd6714073d
zuishen <zshen.usc@gmail.com>
2020-07-03 19:25:23 +0800

    Solve p5

--- src/main/java/work/jimmmy/prep/exam1/Solutions5.java
+++ src/main/java/work/jimmmy/prep/exam1/Solutions5.java
@@ -2,7 +2,7 @@ package work.jimmmy.prep.exam1;
 
 public class Solutions5 {
 
-    public String longestPalindrome(String str) {
+    public String longestPalindrome1(String str) {
         if (str == null || str.length() == 0) {
             return str;
         }
@@ -15,6 +15,30 @@ public class Solutions5 {
                     result = substring;
                     maxLength = substring.length();
                 }
+            }
+        }
+        return result;
+    }
+
+    public String longestPalindrome(String str) {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        int maxLength = 0;
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            String oddPalindrome = getLongestPalindrome(str, i, i);
+            if (maxLength < oddPalindrome.length()) {
+                maxLength = oddPalindrome.length();
+                result = oddPalindrome;
+            }
+            if (i + 1 >= str.length()) {
+                continue;
+            }
+            String evenPalindrome = getLongestPalindrome(str, i, i + 1);
+            if (maxLength < evenPalindrome.length()) {
+                maxLength = evenPalindrome.length();
+                result = evenPalindrome;
             }
         }
         return result;
@@ -43,7 +67,25 @@ public class Solutions5 {
         return true;
     }
 
+    /**
+     * 获取长回文串
+     *
+     * 调用保证参数的有效性
+     *
+     * @param str
+     * @param left
+     * @param right
+     * @return
+     */
     private String getLongestPalindrome(String str, int left, int right) {
-        return "";
+        int lp = left;
+        int rp = right;
+
+        while (lp >= 0 && rp < str.length()) {
+            if (str.charAt(lp) != str.charAt(rp)) {
+                return lp + 1 < rp - 1 ? str.substring(lp + 1, rp) : str.substring(left, left + 1);
+            }
+        }
+        return str.substring(lp + 1, rp);
     }
 }
